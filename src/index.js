29f138dc94fd1bac1228da328d5c483ad11025e9
chenwenxu <2229313737@qq.com>
2020-12-27 03:16:08 +0800

    幸会记-鸦青

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const api = require('./api');
+const demo = require('./demo');
+
+app.use('/api', api); // api路由
+app.use('/demo', demo); // demo路由
+app.use('/yu', express.static(path.join(__dirname, '../static'))); // 静态资源
+
+// 首页
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../static', 'index.html'));
+});
+
+// 404处理
+app.all('*', (req, res) => {
+  res.send('茶π挺好喝的');
+})
+
+// 3000端口
+app.listen(3000, () => {
+  console.log('port 3000');
+});
